5a7fdb43baf43daad18d86cc8d22f0b5c4136928
root <root@atecubanos.heitor.senaisp.br>
2017-03-26 08:58:44 -0300

    Contar até 10 segundos e finalizar

---
+++ contandoatedez.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#Auto: Heitor
+#Data da criação: 10/02/2017
+#Horário da modificação: 08:21hs
+#Script para contar até 10 segundos e finalizar
+
+
+clear		#limpa a tela.
+
+echo -n "Contarei até 10 segundos: "  #imprime a mensagem dentro das aspas.
+echo -n "."		  #imprime a próxima mensagem na mesma linha (-n).
+sleep 1			#aguarda 1 segundo
+echo -n "."
+sleep 1
+echo -n "."
+sleep 1
+echo -n "."
+sleep 1
+echo -n "."
+sleep 1
+echo -n "."
+sleep 1
+echo -n "."
+sleep 1
+echo -n "."
+sleep 1
+echo -n "."
+sleep 1
+echo -n "."
+echo      #pula uma linha depois do final do script.
