d9e761b6f1778ff3ac45f631dd55db3339e4e375
root <root@atecubanos.heitor.senaisp.br>
2017-03-26 08:46:03 -0300

    Primeiro script - Olá mundo!

---
+++ olamundo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Autor: Heitor
+# Data da criação: 10/02/2017
+# Horário da modificação: 08:00hrs
+# Primeiro script - Exibir mensagem na tela.
+
+clear	 #limpa a tela
+echo "Olá Mundo!"	 #imprime a mensagem que está dentro das aspas
+sleep 1		  #aguarda 1 segundo até a proxíma ação
+echo "Sejam bem vindos (as)!"
+sleep 1
+echo "O programa terminou." 
+
