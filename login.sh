f92bb5a7bbcf2ca60b7c15f3b5b2c6faab59a3cb
root <root@atecubanos.heitor.senaisp.br>
2017-03-26 09:36:54 -0300

    Simulando a tela de login do Debian

--- login.sh
+++ login.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-USER=vagrant
-SENHA=vagrant
+USER=gokubr
+SENHA=123456
+SAIR=a
 
+while [ $SAIR != b ]; do
+
+QNT=1
 
 clear
+
+while (($QNT <= 5)); do
 
 echo
 echo "Debian GNU/Linux jessie 8 tty1"
@@ -15,21 +21,25 @@ echo -n "Password: "
 read -s PASS
 echo
 
-
 if [ $LOGIN == $USER ]; then
    if [ $PASS == $SENHA ]; then     
-     
+     SAIR=b     
+     QNT=6
+     echo
      echo "Seja bem vindo(a)!"  
-
+     echo
 else
          sleep 2 
 	 echo 
 	 echo "Login incorrect"
+         echo     
      fi
 else
    	sleep 2 
         echo
 	echo "Login incorrect"
-
-      fi 
-
+        echo  
+fi 
+   let QNT=($QNT+1)
+ done
+done
