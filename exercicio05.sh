62c79033b6218d044cdd111c4cf8f971e2446fd3
root <root@jessie.raw>
2017-03-31 10:23:04 +0000

    teste

---
+++ exercicio05.sh
@@ -0,0 +1,27 @@
+#!bin/bash
+
+DIG=1; PRI=0; SEG=0; TER=0;
+
+clear
+while (( $DIG !=0 )); do
+        echo "Digite seu salário:"
+  	read DIG; SAL=$DIG
+       [ $SAL == $PRI ] && SAL=0
+       [ $SAL == $SEG ] && SAL=0
+    if (( $SAL > $PRI )); then
+        TER=$SEG
+        SEG=$PRI
+        PRI=$SAL
+     else
+           if (( $SAL > $SEG )); then
+               TER=$SEG
+               SEG=$SAL
+            else
+                 if (( $SAL >= $TER )); then
+                      TER=$SAL
+                  fi
+           fi
+   fi
+done
+
+echo "Os maiores valores respectivamente são $PRI , $SEG , $TER."
