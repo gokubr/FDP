a35e308249dd1e714cbde4207a2d6655c8a541a3
gokubr <heitoraparicio@live.com>
2017-04-07 04:47:56 -0300

    Resolução da atividade

---
+++ exercicio02.sh
@@ -0,0 +1,24 @@
+######### II. Faça um programa para solicitar a quantidade de números inteiros a serem
+lidos, em seguida ler os números e escrever o maior número dentre os
+informados.##########
+
+#!binbash
+
+clear
+
+a=0
+b=0
+
+echo -n  "Digite a quantidade de números inteiros a serem lidos: "
+read c
+
+while (( $c > $a )); do
+   echo "Digite um número: "
+   read n
+       if (( $n > $b )); then 
+      	b=$n
+	  	
+      fi
+        let a=($a+1)
+done	
+echo "O maior número digitado foi: $b"
